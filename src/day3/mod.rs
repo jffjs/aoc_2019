facfbb9dc57f4d059ddbc1c8d4fbb325183c3d1d
Jeff Smith <jsmith3@vonage.com>
2019-12-04 09:37:36 -0500

    day 3, part 2

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -38,14 +38,19 @@ pub fn day_3() {
 
     let mut grid = Grid::new();
 
-    for (wire, line_) in reader.lines().enumerate() {
+    let wire_paths: Vec<Vec<Segment>> = reader
+        .lines()
+        .map(|l| {
+            let line = l.unwrap();
+            line.trim()
+                .split(',')
+                .map(|segment| Segment::new(&segment))
+                .collect()
+        })
+        .collect();
+
+    for (wire, path) in wire_paths.iter().enumerate() {
         let wire = wire + 1;
-        let line = line_.unwrap();
-        let path: Vec<Segment> = line
-            .trim()
-            .split(',')
-            .map(|segment| Segment::new(&segment))
-            .collect();
 
         let mut position: Coord = (0, 0);
 
@@ -101,6 +106,82 @@ pub fn day_3() {
     println!("Day 3-1: {}", shortest_distance);
 
     // Part 2
+    let intersections: Vec<Coord> = grid
+        .iter()
+        .filter(|(_, wires)| wires.len() > 1)
+        .map(|(pos, _)| *pos)
+        .collect();
+
+    let mut shortest_steps = 999_999;
+    for intersection in intersections.iter() {
+        let mut total_steps = 0;
+        for path in wire_paths.iter() {
+            let mut position: Coord = (0, 0);
+            let mut steps = 0;
+            for segment in path.iter() {
+                match segment {
+                    Segment::U(y) => {
+                        let x = position.0;
+                        let sy = position.1;
+                        for dy in 1..=*y {
+                            position = (x, sy + dy);
+                            steps += 1;
+
+                            if position == *intersection {
+                                total_steps += steps;
+                                break;
+                            }
+                        }
+                    }
+                    Segment::D(y) => {
+                        let x = position.0;
+                        let sy = position.1;
+                        for dy in 1..=*y {
+                            position = (x, sy - dy);
+                            steps += 1;
+
+                            if position == *intersection {
+                                total_steps += steps;
+                                break;
+                            }
+                        }
+                    }
+                    Segment::R(x) => {
+                        let sx = position.0;
+                        let y = position.1;
+                        for dx in 1..=*x {
+                            position = (sx + dx, y);
+                            steps += 1;
+
+                            if position == *intersection {
+                                total_steps += steps;
+                                break;
+                            }
+                        }
+                    }
+                    Segment::L(x) => {
+                        let sx = position.0;
+                        let y = position.1;
+                        for dx in 1..=*x {
+                            position = (sx - dx, y);
+                            steps += 1;
+
+                            if position == *intersection {
+                                total_steps += steps;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if total_steps < shortest_steps {
+            shortest_steps = total_steps;
+        }
+    }
+
+    println!("Day 3-2: {}", shortest_steps);
 }
 
 fn mark_grid(grid: &mut Grid, pos: Coord, wire: usize) {
