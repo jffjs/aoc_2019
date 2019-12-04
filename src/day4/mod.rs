151e6fd467125894bc8d5e34ddf51da7b40ef4e2
Jeff Smith <jsmith3@vonage.com>
2019-12-04 11:40:57 -0500

    day 4, part 2

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -14,6 +14,23 @@ pub fn day_4() {
     }
 
     println!("Day 4-1: {}", possible_codes);
+
+    // part 2
+    let mut possible_codes = 0;
+    for code in start..=end {
+        let code_str = &format!("{}", code);
+        if has_two_adjacent(code_str) && does_not_decrease(code_str) {
+            let groups = get_groups(code_str);
+            let groups_of_two: Vec<&String> =
+                groups.iter().filter(|group| group.len() == 2).collect();
+
+            if groups_of_two.len() >= 1 {
+                possible_codes += 1;
+            }
+        }
+    }
+
+    println!("Day 4-1: {}", possible_codes);
 }
 
 fn has_two_adjacent(code: &str) -> bool {
@@ -40,6 +57,49 @@ fn does_not_decrease(code: &str) -> bool {
     true
 }
 
+fn find_group(idx: usize, code: &str, mut group: String) -> String {
+    if let Some(c) = code.chars().nth(idx) {
+        if let Some(gc) = group.chars().nth(group.len() - 1) {
+            if gc == c {
+                group.push(c);
+                group = find_group(idx + 1, code, group);
+            }
+        }
+    }
+    group
+}
+
+fn get_groups(code: &str) -> Vec<String> {
+    let mut groups: Vec<String> = Vec::new();
+    // for (i, c) in code.chars().enumerate() {
+    //     let mut group = c.to_string();
+    //     group = find_group(i + 1, code, group);
+
+    //     if group.len() > 1 {
+    //         groups.push(group);
+    //     }
+    // }
+
+    let mut i = 0;
+    loop {
+        if i >= code.len() {
+            break;
+        }
+
+        let c = code.chars().nth(i).unwrap();
+        let mut group = c.to_string();
+        group = find_group(i + 1, code, group);
+
+        let group_len = group.len();
+        if group_len > 1 {
+            groups.push(group);
+        }
+        i += group_len;
+    }
+
+    groups
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -62,5 +122,46 @@ mod tests {
         assert_eq!(does_not_decrease("111222"), true);
         assert_eq!(does_not_decrease("999998"), false);
         assert_eq!(does_not_decrease("999990"), false);
+    }
+
+    #[test]
+    fn test_find_group() {
+        assert_eq!(
+            find_group(1, "11234", String::from("1")),
+            String::from("11")
+        );
+        assert_eq!(
+            find_group(1, "1111234", String::from("1")),
+            String::from("1111")
+        );
+        assert_eq!(
+            find_group(3, "1122234", String::from("2")),
+            String::from("222")
+        );
+        assert_eq!(
+            find_group(5, "112344", String::from("4")),
+            String::from("44")
+        );
+    }
+
+    #[test]
+    fn test_get_groups() {
+        assert_eq!(
+            get_groups("112233"),
+            vec!["11".to_owned(), "22".to_owned(), "33".to_owned()]
+        );
+        assert_eq!(
+            get_groups("1122233"),
+            vec!["11".to_owned(), "222".to_owned(), "33".to_owned()]
+        );
+        assert_eq!(
+            get_groups("1232233"),
+            vec!["22".to_owned(), "33".to_owned()]
+        );
+        assert_eq!(
+            get_groups("1232233"),
+            vec!["22".to_owned(), "33".to_owned()]
+        );
+        assert_eq!(get_groups("111111"), vec!["111111".to_owned()]);
     }
 }
