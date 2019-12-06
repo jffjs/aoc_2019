62b2437d80fdc4d7d18709c58badc4e44336f6ea
Jeff Smith <jsmith3@vonage.com>
2019-12-06 13:29:31 -0500

    clean up some code

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -6,7 +6,7 @@ pub fn day_6() {
     let file = read_input(6);
     let reader = BufReader::new(file);
 
-    let mut leaf_to_parent: HashMap<String, String> = HashMap::new();
+    let mut orbits: HashMap<String, String> = HashMap::new();
 
     for line in reader.lines() {
         let line = line.unwrap();
@@ -15,57 +15,31 @@ pub fn day_6() {
         let orbitee = split[0];
         let orbiter = split[1];
 
-        leaf_to_parent.insert(orbiter.to_owned(), orbitee.to_owned());
+        orbits.insert(orbiter.to_owned(), orbitee.to_owned());
     }
 
     // Part 1
     let mut checksum = 0;
-    for node in leaf_to_parent.keys() {
-        checksum += reverse_tree_walk(&leaf_to_parent, node);
+    for node in orbits.keys() {
+        checksum += get_hops_to_ancestor(&orbits, node, "COM");
     }
 
     println!("Day 6-1: {}", checksum);
 
     // Part 2
-    let ancestor = find_first_common_ancestor(&leaf_to_parent, "YOU", "SAN");
+    let ancestor = find_first_common_ancestor(&orbits, "YOU", "SAN");
     println!(
         "Day 6-2: {}",
-        get_hops_to_ancestor(
-            &leaf_to_parent,
-            leaf_to_parent.get("YOU").unwrap(),
-            &ancestor
-        ) + get_hops_to_ancestor(
-            &leaf_to_parent,
-            leaf_to_parent.get("SAN").unwrap(),
-            &ancestor
-        )
+        get_hops_to_ancestor(&orbits, orbits.get("YOU").unwrap(), &ancestor)
+            + get_hops_to_ancestor(&orbits, orbits.get("SAN").unwrap(), &ancestor)
     );
 }
 
-fn reverse_tree_walk(leaf_to_parent: &HashMap<String, String>, start: &str) -> usize {
-    let mut hops = 0;
-
-    let mut node = start;
-    loop {
-        if let Some(parent) = leaf_to_parent.get(node) {
-            hops += 1;
-            node = parent;
-        } else {
-            break;
-        }
-    }
-
-    hops
-}
-
-fn get_ancestors<'a>(
-    leaf_to_parent: &'a HashMap<String, String>,
-    mut node: &'a str,
-) -> HashSet<&'a str> {
+fn get_ancestors<'a>(orbits: &'a HashMap<String, String>, mut node: &'a str) -> HashSet<&'a str> {
     let mut ancestors: HashSet<&str> = HashSet::new();
 
     loop {
-        if let Some(parent) = leaf_to_parent.get(node) {
+        if let Some(parent) = orbits.get(node) {
             ancestors.insert(parent);
             node = parent;
         } else {
@@ -76,13 +50,9 @@ fn get_ancestors<'a>(
     ancestors
 }
 
-fn find_first_common_ancestor(
-    leaf_to_parent: &HashMap<String, String>,
-    a: &str,
-    b: &str,
-) -> String {
-    let ancestors_a = get_ancestors(leaf_to_parent, a);
-    let ancestors_b = get_ancestors(leaf_to_parent, b);
+fn find_first_common_ancestor(orbits: &HashMap<String, String>, a: &str, b: &str) -> String {
+    let ancestors_a = get_ancestors(orbits, a);
+    let ancestors_b = get_ancestors(orbits, b);
 
     let intersection: Vec<&str> = ancestors_a
         .intersection(&ancestors_b)
@@ -92,7 +62,7 @@ fn find_first_common_ancestor(
     let mut fewest_hops = usize::max_value();
     let mut ancestor: &str = "";
     for node in intersection.iter() {
-        let hops = get_hops_to_ancestor(leaf_to_parent, a, node);
+        let hops = get_hops_to_ancestor(orbits, a, node);
         if hops < fewest_hops {
             fewest_hops = hops;
             ancestor = node;
@@ -102,16 +72,12 @@ fn find_first_common_ancestor(
     ancestor.to_owned()
 }
 
-fn get_hops_to_ancestor(
-    leaf_to_parent: &HashMap<String, String>,
-    start: &str,
-    ancestor: &str,
-) -> usize {
+fn get_hops_to_ancestor(orbits: &HashMap<String, String>, start: &str, ancestor: &str) -> usize {
     let mut hops = 0;
 
     let mut node = start;
     loop {
-        if let Some(parent) = leaf_to_parent.get(node) {
+        if let Some(parent) = orbits.get(node) {
             hops += 1;
             node = parent;
 
